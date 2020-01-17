3f1e0d4a45a0517e87abc27ccd8a4282dfeeab55
gulnaztanbay <gulnaztanbay@gmail.com>
2020-01-17 15:40:06 +0300

    Cross movement of object blocked.

--- Assets/ItemMovement.cs
+++ Assets/ItemMovement.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class ItemMovement : MonoBehaviour
 {
-
-    private Vector3 _screenPoint;
-    private Vector3 _offset;
-    Vector3 curScreenPoint, curPosition;
-
+    private Vector3 _screenPoint, _offset,_curScreenPoint, _curPosition, _firstScreenPoint;
     private Vector3Int _cellPosition, _itemPosition;
+
     private GameController _gameContObj;
     private GameObject _item;
 
+    private MovementEnum _movementEnum;
+    private bool _mouseDownOnce = true;
+    private bool _moveHorizontal, _moveVertical = false;
+
+
     private void Start()
     {
+        _movementEnum = MovementEnum.EMPTY;
         _gameContObj = GameObject.Find("GameController").GetComponent<GameController>();
         _item = GameObject.Find("Cube");
 
@@ -23,36 +26,92 @@ public class ItemMovement : MonoBehaviour
 
     private void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        switch (_movementEnum)
         {
-            _itemPosition = _gameContObj._grid.LocalToCell(_item.transform.position);
+            case MovementEnum.CLICK:
+                _mouseDownOnce = true;
 
-            Debug.Log(_itemPosition);
-            _screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-            _offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
+                _itemPosition = _gameContObj._grid.LocalToCell(_item.transform.position);
+                _firstScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+                _screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+                _offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
+                _movementEnum = MovementEnum.MOVE;
+                break;
+
+
+            case MovementEnum.MOVE:
+
+                _curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+                _curPosition = Camera.main.ScreenToWorldPoint(_curScreenPoint) + _offset;
+
+                if (_moveHorizontal)
+                    transform.position = new Vector3(_curPosition.x, transform.position.y, transform.position.z);
+
+                if (_moveVertical)
+                    transform.position = new Vector3(transform.position.x, transform.position.y, _curPosition.z);
+
+                _cellPosition = _gameContObj._grid.LocalToCell(transform.position);
+
+                break;
+            default:
+                break;
         }
+
         if (Input.GetMouseButton(0))
         {
 
-            curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
-            curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + _offset;
-            transform.position = new Vector3(curPosition.x, transform.position.y, curPosition.z);
-            _cellPosition = _gameContObj._grid.LocalToCell(transform.position);
+            if (Mathf.Abs(_firstScreenPoint.x - Input.mousePosition.x) > 20 && _mouseDownOnce)
+            {
+                _mouseDownOnce = false;
+                _moveHorizontal = true;
+                _moveVertical = false;
 
+                if (Mathf.Abs(_firstScreenPoint.y - Input.mousePosition.y) > 20)
+                {
+                    _moveVertical = true;
+                    _moveHorizontal = false;
+                    _movementEnum = MovementEnum.CLICK;
+                }
+                _movementEnum = MovementEnum.CLICK;
+            }
+
+            if (Mathf.Abs(_firstScreenPoint.y - Input.mousePosition.y) > 20 && _mouseDownOnce)
+            {
+                _mouseDownOnce = false;
+                _moveVertical = true;
+                _moveHorizontal = false;
+
+
+                if (Mathf.Abs(_firstScreenPoint.x - Input.mousePosition.x) > 20)
+                {
+                    _moveHorizontal = true;
+                    _moveVertical = false;
+                    _movementEnum = MovementEnum.CLICK;
+                }
+                _movementEnum = MovementEnum.CLICK;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _movementEnum = MovementEnum.EMPTY;
         }
     }
     private void FixedUpdate()
     {
-        //Debug.Log(_cellPosition);
-        if (_itemPosition!= _cellPosition)
+        if (_itemPosition != _cellPosition)
         {
-             _item.transform.position = _gameContObj._grid.CellToLocal(_cellPosition);
+            _item.transform.position = _gameContObj._grid.CellToLocal(_cellPosition);
             _itemPosition = _gameContObj._grid.LocalToCell(_item.transform.position);
             Debug.Log(_itemPosition);
         }
     }
 
 
+
+}
+
+public enum MovementEnum
+{
+    CLICK, MOVE, EMPTY
 
 }
